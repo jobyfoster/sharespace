2484bb44e2d19ee4bc3f6b441bef720f17a74e89
Owen <ocotten24@basecampcodingacademy.org>
2024-01-09 13:50:56 -0600

    added settings page. YIPPPPIEEEEEEEEEEEEE!

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import ShareSpace
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
 
 
 class MultipleFileInput(forms.ClearableFileInput):
@@ -65,3 +67,20 @@ class ShareSpaceAccessForm(forms.Form):
     password = forms.CharField(
         max_length=50, widget=forms.PasswordInput(), required=True
     )
+
+
+class PasswordChangingForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Old Password'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'New Password'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm New Password'}))
+
+    class Meta:
+        model = User
+        fields = ['old_password', 'new_password1', 'new_password2']
+
+class UsernameChangingForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, required=True, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.')
+
+    class Meta:
+        model = User
+        fields = ['username']
