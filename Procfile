c15baa70e73fc98c87629dcf735aae6533fdd0cd
Jordan <jdonahue24@basecampcodingacademt.org>
2024-03-07 10:03:32 -0600

    Update database configuration

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: python manage.py runserver 0.0.0.0:$PORT
-web: gunicorn sharespace.wsgi
-web: python manage.py collectstatic --noinput; gunicorn yourapp.wsgi
--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +79,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-import dj_database_url
-
-# Load database configuration from the DATABASE_URL environment variable
-DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
